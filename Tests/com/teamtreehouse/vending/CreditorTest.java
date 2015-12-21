509947139d89fade64f691267681d86a08960d6f
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2015-12-21 17:37:48 +0000

    Added first test

---
+++ Tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,16 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Steve on 21/12/2015.
+ */
+public class CreditorTest {
+
+    @Test
+    public void testRefund() throws Exception {
+        fail();
+    }
+}
