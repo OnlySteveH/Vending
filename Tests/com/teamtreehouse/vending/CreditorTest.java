519d8cfcae212813b7dfb7f961951215a3ccad4c
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2015-12-22 14:34:09 +0000

    Adding additional tests

--- Tests/com/teamtreehouse/vending/CreditorTest.java
+++ Tests/com/teamtreehouse/vending/CreditorTest.java
@@ -10,7 +10,19 @@ import static org.junit.Assert.*;
 public class CreditorTest {
 
     @Test
-    public void testRefund() throws Exception {
-        fail();
+    public void addingFundsIncrementsAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+        assertEquals(50, creditor.getAvailableFunds());
+    }
+
+    @Test
+    public void refundingReturnsAllAvailableFunds(){
+        Creditor creditor = new Creditor();
+        creditor.addFunds(10);
+        int refund = creditor.refund();
+        assertEquals(10, refund);
+        assertEquals(0, creditor.getAvailableFunds());
     }
 }
