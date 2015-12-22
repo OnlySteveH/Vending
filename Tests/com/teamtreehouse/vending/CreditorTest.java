1ac4986a57d955675b07d80ad42623c431428aa6
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2015-12-22 15:54:55 +0000

    Adding exception management as well as rules

--- Tests/com/teamtreehouse/vending/CreditorTest.java
+++ Tests/com/teamtreehouse/vending/CreditorTest.java
@@ -1,5 +1,7 @@
 package com.teamtreehouse.vending;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,10 +10,16 @@ import static org.junit.Assert.*;
  * Created by Steve on 21/12/2015.
  */
 public class CreditorTest {
+    private Creditor creditor;
+
+    @Before
+    public void setUp() throws Exception {
+        creditor = new Creditor();
+    }
+
 
     @Test
     public void addingFundsIncrementsAvailableFunds() throws Exception {
-        Creditor creditor = new Creditor();
         creditor.addFunds(25);
         creditor.addFunds(25);
         assertEquals(50, creditor.getAvailableFunds());
@@ -19,10 +27,26 @@ public class CreditorTest {
 
     @Test
     public void refundingReturnsAllAvailableFunds(){
-        Creditor creditor = new Creditor();
         creditor.addFunds(10);
         int refund = creditor.refund();
         assertEquals(10, refund);
+    }
+    @Test
+    public void refundingResetsAvailableFundsToZero() {
+        creditor.addFunds(10);
+        creditor.refund();
         assertEquals(0, creditor.getAvailableFunds());
+    }
+
+    @Test(expected = NotEnoughFundsException.class)
+    public void deductingMoreThanBalanceNotAllowed() throws Exception{
+        creditor.addFunds(10);
+        creditor.deduct(20);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+
     }
 }
